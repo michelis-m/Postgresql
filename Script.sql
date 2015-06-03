1297b55bddb796d053dacef34630b14432c4e822
michelis-m <michelis_m@yahoo.com>
2015-06-03 15:06:37 +0100

    Update Script.sql

--- Script.sql
+++ Script.sql
@@ -8,3 +8,24 @@ FROM
 	FROM practise
 ) as cte
 WHERE p.id=cte.id
+
+------------------- Counts ----------------------------------------------------
+
+WITH C AS
+(
+	SELECT dan_sequence_id , ROW_NUMBER() OVER (PARTITION BY dan_sequence_id ORDER BY (SELECT NULL)) AS n
+	FROM public.adjusted_rich_media_metrics1_728656
+)
+SELECT * FROM C WHERE n>1
+
+------------------- Counts 2 ----------------------------------------------------
+
+select count(1),count(distinct dan_sequence_Id) from [schema][dot][table] having count(1)<>count(distinct dan_sequence_Id) ;
+
+
+
+select 'select count(1),count(distinct dan_sequence_Id) from '||table_schema||'.'||table_name||' having count(1)<>count(distinct dan_sequence_Id)'||' union'
+from information_schema.tables
+where table_type = 'BASE TABLE'
+and table_schema = 'public'
+and table_name like 'adjusted_%';
