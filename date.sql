6f13ce1ec0e764ea9b2dc2b7640da8b5611261ff
michelis-m <michelis_m@yahoo.com>
2015-08-24 15:30:24 +0100

    Create date.sql

---
+++ date.sql
@@ -0,0 +1,1 @@
+select to_date(to_char((extract( year from now())) * 10000 + (extract( month from now())) * 100 + (select days from dan_control.task_lookback where task_id = 20105),'00000000'), 'yyyymmdd')
