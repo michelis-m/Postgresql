28c30c6a051b7b4a016dc65c9ea7a6f245be1a08
michelis-m <michelis_m@yahoo.com>
2015-08-24 15:32:59 +0100

    Update date.sql

--- date.sql
+++ date.sql
@@ -1,1 +1,4 @@
 select to_date(to_char((extract( year from now())) * 10000 + (extract( month from now())) * 100 + (select days from dan_control.task_lookback where task_id = 20105),'00000000'), 'yyyymmdd')
+
+
+select to_date(to_char((extract( year from now())) * 10000 + (extract( month from now())) * 100 + (extract( day from now()))- (select days from dan_control.task_lookback where task_id = 20105) ,'00000000'), 'yyyymmdd')
