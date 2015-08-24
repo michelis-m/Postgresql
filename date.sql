3f0f3c6e253e98aa4be73ad3e0dc143570dfc1b5
michelis-m <michelis_m@yahoo.com>
2015-08-24 16:10:40 +0100

    Update date.sql

--- date.sql
+++ date.sql
@@ -2,3 +2,8 @@ select to_date(to_char((extract( year from now())) * 10000 + (extract( month fro
 
 
 select to_date(to_char((extract( year from now())) * 10000 + (extract( month from now())) * 100 + (extract( day from now()))- (select days from dan_control.task_lookback where task_id = 20105) ,'00000000'), 'yyyymmdd')
+
+
+select now()+((days*-1) * interval '1 day'),days,now()
+from dan_control.task_lookback, dan_control.job  
+where task_id = 20105
